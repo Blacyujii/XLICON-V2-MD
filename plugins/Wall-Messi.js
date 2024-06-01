a741dc892a163ed24ce2c3169eea0c381fd2cc35
Blacyujii <privateheroku01@gmail.com>
2024-06-01 17:35:23 +0100

    Add files via upload

---
+++ plugins/Wall-Messi.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/Messi.json`)).data  
+let url = await res[Math.floor(res.length * Math.random())]
+conn.sendFile(m.chat, url, 'error.jpg', `*🐐*`, m)} 
+//conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
+handler.help = ['messi']
+handler.tags = ['img']
+handler.command = /^(messi)$/i
+export default handler
